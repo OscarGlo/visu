a76e9bb410634b7dc33a0e99a22111cf511a2c6c
OscarGlo <gloaguen.oscar@gmail.com>
2021-02-02 12:10:07 +0100

    Offset spirals

--- script.js
+++ script.js
@@ -31,17 +31,19 @@ function draw() {
 
 function drawSpiral(o) {
     let x = canvas.width / 2;
-    for (let y = 0; y < canvas.height; y += 3) {
+    for (let y = 0; y <= canvas.height; y += 15) {
         let a = y / 40 + o;
-        let xx = Math.sin(a) * 80;
-        let z = Math.cos(a) * 40;
-        ctx.fillStyle = `hsla(${(hue - 180) % 360}, 50%, 50%, ${(50 + z) / 100}`;
+
+        ctx.fillStyle = `hsla(${(hue - 180) % 360}, 50%, 50%, ${(50 + Math.cos(a) * 40) / 100})`;
         ctx.beginPath();
-        ctx.arc(x + xx, y, 6, 0, 2 * Math.PI);
+        ctx.arc(x + Math.sin(a) * 80, y, 6, 0, 2 * Math.PI);
         ctx.fill();
-        ctx.fillStyle = `hsla(${(hue - 180) % 360}, 50%, 50%, ${(50 - z) / 100}`;
+
+        a = y / 40 + o / 2;
+
+        ctx.fillStyle = `hsla(${(hue - 180) % 360}, 50%, 50%, ${(50 - Math.cos(a) * 40) / 100})`;
         ctx.beginPath();
-        ctx.arc(x - xx, y, 6, 0, 2 * Math.PI);
+        ctx.arc(x - Math.sin(a) * 80, y, 6, 0, 2 * Math.PI);
         ctx.fill();
     }
 }
@@ -49,5 +51,5 @@ function drawSpiral(o) {
 function update() {
     hue = (hue + 0.1) % 360;
     bgColor = `hsl(${hue}, 50%, 10%)`;
-    offset += 0.02;
+    offset -= 0.03;
 }
