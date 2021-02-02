0a7aa0f34d9e36a007f9a134cf38eb6796fd8485
OscarGlo <oscarglo@free.fr>
2021-02-02 17:24:59 +0100

    Configuration

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let canvas, ctx;
+let canvas, controls, ctx;
 
 function fit() {
     [canvas.width, canvas.height] = [window.innerWidth, window.innerHeight];
@@ -6,6 +6,7 @@ function fit() {
 
 window.addEventListener("load", () => {
     canvas = document.querySelector("canvas");
+    controls = document.getElementById("controls");
     ctx = canvas.getContext("2d");
 
     fit();
@@ -17,6 +18,18 @@ window.addEventListener("load", () => {
         update();
         draw();
     }, 15);
+
+    window.addEventListener("keydown", evt => {
+        if (evt.code === "KeyC") {
+            if (controls.style.display === "block") {
+                controls.style.display = "none";
+                document.body.style.cursor = "none";
+            } else {
+                controls.style.display = "block";
+                document.body.style.cursor = "default";
+            }
+        }
+    });
 });
 
 let hue = Math.random() * 360, bgColor;
@@ -26,26 +39,22 @@ function draw() {
     ctx.fillStyle = bgColor;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    drawSpiral(offset);
+    function vi(id) {
+        return parseFloat(document.getElementById(id).value);
+    }
+
+    drawHelix(offset, vi("n"), vi("d"), vi("w"), vi("f"), vi("r"));
 }
 
-function drawSpiral(o) {
-    let x = canvas.width / 2;
-    for (let y = 0; y <= canvas.height; y += 15) {
-        let a = y / 40 + o;
-
-        ctx.fillStyle = `hsla(${(hue - 180) % 360}, 50%, 50%, ${(50 + Math.cos(a) * 40) / 100})`;
-        ctx.beginPath();
-        ctx.arc(x + Math.sin(a) * 80, y, 6, 0, 2 * Math.PI);
-        ctx.fill();
-
-        a = y / 40 + o / 2;
-
-        ctx.fillStyle = `hsla(${(hue - 180) % 360}, 50%, 50%, ${(50 - Math.cos(a) * 40) / 100})`;
-        ctx.beginPath();
-        ctx.arc(x - Math.sin(a) * 80, y, 6, 0, 2 * Math.PI);
-        ctx.fill();
-    }
+function drawHelix(o, n, d, w, f, r) {
+    for (let y = -r; y <= canvas.height + r; y += d)
+        for (let i = 0; i < n; i++) {
+            let a = y / f + o + i * 2 * Math.PI / n;
+            ctx.fillStyle = `hsla(${(hue - 180) % 360}, 50%, 50%, ${(50 + Math.cos(a) * 40) / 100})`;
+            ctx.beginPath();
+            ctx.arc(canvas.width / 2 + Math.sin(a) * w, y, r, 0, 2 * Math.PI);
+            ctx.fill();
+        }
 }
 
 function update() {
